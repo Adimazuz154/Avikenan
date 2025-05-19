35d4d8d52546b6a9b5c39fefaa36b9b938007f57
Adimazuz154 <Adimazuz03@gmail.com>
2025-05-19 14:12:52 +0300

    Update scriptAvi.js

--- scriptAvi.js
+++ scriptAvi.js
@@ -96,7 +96,7 @@
     initWheel();
     if (io) io.disconnect();
 
-    const items = document.querySelectorAll("#gallery a, #gallery img");
+    const items = document.querySelectorAll("#gallery a");
     if (!items.length) return;
 
     io = new IntersectionObserver((entries) => {
